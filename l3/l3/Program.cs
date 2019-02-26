751de079c42cebf6ee8e05f5ee8797ed169237ca
juziem <lus0800@mail.ru>
2019-02-26 11:59:50 +0700

    menu

---
+++ l3/l3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key, a, b, res;
+            Console.WriteLine("Vvedite nomer komandi:\n1. Vvedite A \n2. Vvedite B\n3. Vvedite +\n4. Vvedite -\n5. Vvedite *\n6. Vvedite /");
+            key = Console.Read();
+            switch (key)
+            {
+                case 1: 
+                case 2: 
+                case 3: 
+                case 4:
+                case 5:
+                case 6: 
+                default: break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
